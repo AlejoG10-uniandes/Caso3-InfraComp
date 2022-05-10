d0a65ee59f485ece6b6a84d20232a7bfb6c56eb9
Daniel Felipe Martinez Osorio <df.martinezo@uniandes.edu.co>
2022-05-10 17:59:10 -0500

    avances cifrado

--- caso3_a.gonzalezd1_df.martinezo/src/clases/Tabla.java
+++ caso3_a.gonzalezd1_df.martinezo/src/clases/Tabla.java
@@ -44,4 +44,55 @@ public class Tabla {
 	 * Estados de los paquetes
 	 */
 	private ArrayList<Integer> estadosPkts = new ArrayList<Integer>();
+
+
+	public Tabla(ArrayList<String>nombres, ArrayList<String>idsPkts, ArrayList<Integer>estadosPkts){
+		this.nombres = nombres;
+		this.estadosPkts = estadosPkts;
+		this.idsPkts = idsPkts;
+	}
+
+	public boolean existeNombre(String nombre){
+		boolean existe = false;
+
+		for(int i = 0 ; i <nombres.size() && !existe ; i++){
+			if(nombres.get(i).equals(nombre)){
+				existe = true;
+			}
+		}
+
+		return existe;
+
+	}
+
+	public int nombrePaquete(String nombre, String idPaquete){
+
+		boolean termine = false;
+		int indiceNombre=0;
+		int indiceRespuesta=-1;
+
+
+		for(int i = 0; i < nombres.size() && !termine; i++){
+
+			if(nombres.get(i).equals(nombre)){
+				indiceNombre = i;
+				termine = true;
+				
+			}
+			
+		}
+
+		if(idsPkts.get(indiceNombre).equals(idPaquete)){
+			indiceRespuesta = indiceNombre;
+		}
+
+
+
+		return indiceRespuesta;
+
+	}
+
+	public ArrayList<Integer> darTablaEstados(){
+		return estadosPkts;
+	}
 }
--- caso3_a.gonzalezd1_df.martinezo/src/sockets/Cliente.java
+++ caso3_a.gonzalezd1_df.martinezo/src/sockets/Cliente.java
@@ -6,9 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.security.*;
 import java.security.spec.*;
+import java.util.Base64;
+import java.util.Scanner;
+
 import javax.crypto.*;
 
 public class Cliente extends Conexion {
@@ -35,6 +39,14 @@ public class Cliente extends Conexion {
 	private BufferedReader bf;
 	
 	private PublicKey publicKeyServer;
+
+	private SecretKey llaveSimetrica;
+
+	private String reto;
+
+	private String nombre;
+
+	private String estadoPaquete;
 	
 	/**
 	 * -------
@@ -58,6 +70,8 @@ public class Cliente extends Conexion {
 	 */
 	public void leerPkServidor() throws IOException {
 		
+
+
 		File publicKeyFile = new File("public.key");
 		byte[] publicKeyBytes = Files.readAllBytes(publicKeyFile.toPath());
 		
@@ -88,16 +102,19 @@ public class Cliente extends Conexion {
 		pw.println("INICIO");
 		pw.flush();
 		
-		String str = bf.readLine();
-		System.out.println("Servidor: " + str + "\n");
+		// String str = bf.readLine();
+		// System.out.println("Servidor: " + str + "\n");
 	}
 	
 	/**
 	 * Paso 3: El Cliente envia el reto al Servidor (numero aleatorio de 24 digitos)
 	 * @param l, longitud del reto
+	 * @throws IOException
 	 */
-	public void enviarReto(int l) {
-		String reto = "reto -> ";
+	public void enviarReto(int l) throws IOException {
+		String str = bf.readLine();
+
+		String retoMensaje = "reto -> ";
 		int min = 0;
 		int max = 9;
 		int rnd = 0;
@@ -107,14 +124,242 @@ public class Cliente extends Conexion {
 			
 			// (int) (Math.random() * max) + min
 			rnd = (int) (Math.random() * max) + min;
-			
-			reto += rnd;
+			reto+=rnd+"";
+			retoMensaje += rnd;
 		}
 		
-		pw.println(reto);
+		pw.println(retoMensaje);
 		pw.flush();
 	}
+
+	/**
+	 * Paso 4: El Cliente valida si el reto encriptado es igual al generado, si es correcto se genera la llave simétrica
+	 * y se envía cifrada con la llave pública al Servidor
+	 * @throws NoSuchAlgorithmException
+	 */
 	
+	public void validacionRetoEncriptado() throws IOException, NoSuchAlgorithmException {
+
+		String str = bf.readLine();
+		
+		String retoDescifrado = descifrarPublica(str);
+
+			if(!reto.equals(retoDescifrado)){
+
+				terminarConexion();
+
+			}
+		// genera llave simetrica de 256 bits
+			generateKey(256);
+
+			Cipher encryptCipher;
+				try {
+					encryptCipher = Cipher.getInstance("RSA");
+					encryptCipher.init(Cipher.ENCRYPT_MODE, publicKeyServer);
+
+					byte[] encryptedKey = encryptCipher.doFinal(llaveSimetrica.getEncoded());
+					String encodedKey = Base64.getEncoder().encodeToString(encryptedKey);
+
+					// envío llave simetrica encriptada RSA
+					pw.println("llave simetrica: " + encodedKey);
+					pw.flush();
+
+
+				} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+
+		pw.flush();
+
+	}
+
+	/**
+	 * Paso 5: Se envia el nombre y se espera confirmacion para seguir con el id de paquete
+	 * @throws IOException
+	 */
+	public void envioNombre() throws IOException{
+
+		String str = bf.readLine();
+
+		
+		
+		if(str.equals("ACK")){
+
+			Scanner reader = new Scanner(System.in);  
+
+			System.out.println("Escriba su nombre: ");
+			nombre = reader.nextLine();
+
+			String encodedName = cifrarPublica(nombre);
+
+			// envío nombre encriptado RSA
+			pw.println("nombre: " + encodedName);
+			reader.close();
+
+		}
+			
+
+			str = bf.readLine();
+			if(str.equals("ERROR")){
+				terminarConexion();
+			}
+			else{
+				Scanner reader = new Scanner(System.in);  
+
+				System.out.println("Escriba el identificador de paquete: ");
+				String idPaquete = reader.nextLine();
+
+				String encodedPaquete = cifrarConLlaveSimetrica(idPaquete);
+
+				pw.print("idPaquete: " + encodedPaquete);
+				reader.close();
+
+			}
+
+			pw.flush();
+
+		}
+
+		/**
+	 * Paso 6: Se recibe el estado de paquete y se envia "ACK"
+	 * @throws IOException
+	 */
+
+	 public void recibirEstado() throws IOException{
+		String str = bf.readLine();
+		if(str.equals("ERROR")){
+			terminarConexion();
+		}
+
+		estadoPaquete = str;
+		pw.println("ACK");
+
+		pw.flush();
+	 }
+
+
+	 /**
+	 * Paso 7: Se recibe el resumen y se verifica
+	 * @throws IOException
+	 * @throws Exception
+	 */
+	public void recibirResumen() throws IOException{
+		String str = bf.readLine();
+
+		String comparacion;
+		try {
+			comparacion = codigoResumen(estadoPaquete);
+			if(str.equals(comparacion)){
+				pw.println("TERMINAR");
+			}
+			else{
+				terminarConexion();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+
+
+	}
+
+
+
+
+
+	public String codigoResumen(String mensaje) throws Exception {
+
+		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+
+        sha256_HMAC.init(llaveSimetrica);
+
+        return byteArrayToHex(sha256_HMAC.doFinal(mensaje.getBytes("UTF-8")));
+  }
+
+  public static String byteArrayToHex(byte[] a) {
+	StringBuilder sb = new StringBuilder(a.length * 2);
+	for(byte b: a)
+		sb.append(String.format("%02x", b));
+	return sb.toString();
+}
+
+	public String descifrarPublica(String mensaje){
+		Cipher decryptCipher;
+		String mensajeDescifrado = "";
+		try {
+			decryptCipher = Cipher.getInstance("RSA");
+			decryptCipher.init(Cipher.DECRYPT_MODE, publicKeyServer);
+
+			byte[] secretMessageBytes = Base64.getDecoder().decode(mensaje);
+
+			byte[] decryptedMessageBytes = decryptCipher.doFinal(secretMessageBytes);
+			mensajeDescifrado = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+		}
+		catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+			return mensajeDescifrado;
+	}
+
+	public String cifrarPublica(String mensaje){
+
+		Cipher encryptCipher;
+		String encodedName = "";
+		try {
+			encryptCipher = Cipher.getInstance("RSA");
+			encryptCipher.init(Cipher.ENCRYPT_MODE, publicKeyServer);
+
+			byte[] secretMessageBytes = mensaje.getBytes(StandardCharsets.UTF_8);
+			byte[] encryptedMessageBytes = encryptCipher.doFinal(secretMessageBytes);
+			encodedName = Base64.getEncoder().encodeToString(encryptedMessageBytes);
+
+
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return encodedName;
+
+	}
+
+	public String cifrarConLlaveSimetrica(String mensaje){
+    
+    Cipher cipher;
+
+	String respuesta="";
+
+	try {
+
+		cipher = Cipher.getInstance("AES/EBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, llaveSimetrica);
+    	byte[] cipherText = cipher.doFinal(mensaje.getBytes());
+		respuesta = Base64.getEncoder().encodeToString(cipherText);
+	} 
+	catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
+    return respuesta;
+
+
+
+	}
+
+	public void generateKey(int n) throws NoSuchAlgorithmException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(n);
+		llaveSimetrica = keyGenerator.generateKey();
+	}
+
 	public void terminarConexion() throws IOException {
 		pw.println("END");
 		pw.flush();
@@ -125,9 +370,11 @@ public class Cliente extends Conexion {
 	/**
 	 * Inicia el socket del Cliente
 	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
 	 */
-	public void start() throws IOException
+	public void start() throws IOException, NoSuchAlgorithmException
     {	
+		reto="";
 		// paso 1
 		leerPkServidor();
 		
@@ -136,6 +383,18 @@ public class Cliente extends Conexion {
 		
 		// paso 3
 		enviarReto(24);
+
+		// paso 4
+		validacionRetoEncriptado();
+
+		// paso 5
+		envioNombre();
+
+		// paso 6
+		recibirEstado();
+
+		// paso 7
+		recibirResumen();
 		
 		// TODO: DEMAS PASOS
 		
